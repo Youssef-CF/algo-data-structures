1fd706d2ad8bb6a6b200a2a67eb16a68f71cca74
Youssef-CF <youssef@craftfoundry.tech>
2021-01-01 13:45:01 +0100

    add unit tests for isEmpty

--- test/test_linkedlist.cpp
+++ test/test_linkedlist.cpp
@@ -13,16 +13,34 @@ TEST_CASE( "search linked list with single element", "[search]" ) {
     Node* n = new Node();
     n->data = 56;
     n->next = nullptr;
-    REQUIRE( search(n, 56) == n);
-    REQUIRE( search(n, 55) == nullptr);
+    REQUIRE( search(n, 56) == n );
+    REQUIRE( search(n, 55) == nullptr );
 }
 
 TEST_CASE( "search linked list with 2 elements", "[search]" ) {
     Node* n1 = new Node(); n1->data = 1;
     Node* n2 = new Node(); n2->data = 2;
     n1->next = n2;
-    REQUIRE( search(n1, 1) == n1);
-    REQUIRE( search(n1, 2) == n2);
-    REQUIRE( search(n1, 3) == nullptr);
+    REQUIRE( search(n1, 1) == n1 );
+    REQUIRE( search(n1, 2) == n2 );
+    REQUIRE( search(n1, 3) == nullptr );
 }
+
+TEST_CASE("isEmpty empty list", "[isEmpty]") {
+    REQUIRE( isEmpty(nullptr) );
+}
+
+TEST_CASE("isEmpty list with one element", "[isEmpty]") {
+    Node* n = new Node();
+    REQUIRE_FALSE( isEmpty(n) );
+}
+
+TEST_CASE("isEmpty list with two elements", "[isEmpty]") {
+    Node* n1 = new Node();n1->data = 1;
+    Node* n2 = new Node();n2->data = 2;
+    n1->next = n2;
+    REQUIRE_FALSE( isEmpty(n1) );
+}
+
+
 
